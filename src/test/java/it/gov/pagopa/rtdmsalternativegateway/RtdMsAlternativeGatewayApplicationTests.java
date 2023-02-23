48bd79dfa742a374eadb53bfffeeea8d02452585
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2023-02-23 11:21:49 +0100

    chore: [RTD-1379] setup repo (#1)
    * [RTD-1379] setup repository
    
    * [RTD-1379] add dockerfile

---
+++ src/test/java/it/gov/pagopa/rtdmsalternativegateway/RtdMsAlternativeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.rtdmsalternativegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtdMsAlternativeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
